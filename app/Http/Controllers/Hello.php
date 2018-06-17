11f25861858bfbd14b2eefdc259f87ba8890aa72
張旭 <zx1986@gmail.com>
2018-06-17 20:48:18 +0800

    https://tutorials.kode-blog.com/laravel-hello-world

--- app/Http/Controllers/Hello.php
+++ app/Http/Controllers/Hello.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
 class Hello extends Controller
 {
     public function index()
